b2fab92b4c118d9bd29907a91241389d6c66d46d
swathi <swathysidharthan@gmail.com>
2023-01-16 12:56:53 +0100

    add 4 more sections

--- tiny-blog/src/App.tsx
+++ tiny-blog/src/App.tsx
@@ -33,6 +33,74 @@ function App() {
             }
         </ul>
       </section>
+
+      <section className='posts-section'>
+        <h2>Mystery</h2>
+        <ul className="posts-mystery">
+            {
+              postData.filter(post => post.tags.includes("mystery")).slice(0, 5)
+              .map(post => 
+                <li key={post.postId} className="posts-list">
+                  <article className='posts-article'>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <footer>{post.tags.map(tag => <span>{tag + " "} </span>)}</footer>
+                  </article>
+                </li>)
+            }
+        </ul>
+      </section>
+
+      <section className='posts-section'>
+        <h2>Fiction</h2>
+        <ul className="posts-fiction">
+            {
+              postData.filter(post => post.tags.includes("fiction")).slice(0, 5)
+              .map(post => 
+                <li key={post.postId} className="posts-list">
+                  <article className='posts-article'>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <footer>{post.tags.map(tag => <span>{tag + " "} </span>)}</footer>
+                  </article>
+                </li>)
+            }
+        </ul>
+      </section>
+
+      <section className='posts-section'>
+        <h2>Classic</h2>
+        <ul className="posts-classic">
+            {
+              postData.filter(post => post.tags.includes("classic")).slice(0, 5)
+              .map(post => 
+                <li key={post.postId} className="posts-list">
+                  <article className='posts-article'>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <footer>{post.tags.map(tag => <span>{tag + " "} </span>)}</footer>
+                  </article>
+                </li>)
+            }
+        </ul>
+      </section>
+
+      <section className='posts-section'>
+        <h2>History</h2>
+        <ul className="posts-history">
+            {
+              postData.filter(post => post.tags.includes("history")).slice(0, 5)
+              .map(post => 
+                <li key={post.postId} className="posts-list">
+                  <article className='posts-article'>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <footer>{post.tags.map(tag => <span>{tag + " "} </span>)}</footer>
+                  </article>
+                </li>)
+            }
+        </ul>
+      </section>
     </div>
   );
 }
