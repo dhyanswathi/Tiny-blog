10fa0dc0c7a1a83194e2de2967f52068606f74a6
swathi <swathysidharthan@gmail.com>
2023-01-16 12:53:28 +0100

    fetch data from api

--- tiny-blog/src/App.tsx
+++ tiny-blog/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { PostInfo } from './types';
 
 function App() {
+  const [postData, setPostData] = useState<PostInfo[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+        const response = await fetch("https://dummyjson.com/posts");
+        const postResults = await response.json();
+        setPostData(postResults.posts);
+    }
+    getData();
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey there! Here is our {postData.length} posts!</h1>
+      
     </div>
   );
 }
