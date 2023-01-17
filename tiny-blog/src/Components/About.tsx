d40a13bb3ccd76b9cb448e700a1c18ce4a278b04
swathi <swathysidharthan@gmail.com>
2023-01-17 18:47:05 +0100

    add about component

---
+++ tiny-blog/src/Components/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+function About() {
+
+    return (
+        <section>
+            <Header></Header>
+            <p>Created by <br/>
+               Swathi Sidharthan 
+            </p>        
+        </section>
+    )
+}
+
+export default About;
--- tiny-blog/src/index.tsx
+++ tiny-blog/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './Components/About';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +14,7 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<App/>} />
-        {/* <Route path="/about" element={<About/>} /> */}
+        <Route path="/about" element={<About/>} />
       </Routes>
     </Router>
   </React.StrictMode>
